3cfb567df4b96090462e5e2bfaa8c3992c3c93b4
Kim Zuniga <kim.eli.zuniga@gmail.com>
2021-11-11 20:50:17 -0500

    Add getdata from sql function

---
+++ clsSQLConnection.php
@@ -0,0 +1,51 @@
+<?php
+
+class clsSQLConnection
+{
+    public function CreateConnection(&$conn)
+    {
+    // Get Heroku ClearDB connection information
+    $cleardb_url            = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server         = $cleardb_url["host"];
+    $cleardb_username       = $cleardb_url["user"];
+    $cleardb_password       = $cleardb_url["pass"];
+    $cleardb_db             = substr($cleardb_url["path"], 1);
+
+    $active_group = 'default';
+    $query_builder = TRUE;
+
+    // Connect to DB
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    }
+
+    public function GetData(&$conn)
+    {
+        $TableName = 'Users';
+        $sessionID = $_COOKIE['userID'];
+        $query = "Select userName, dateTimeStamp, message, sessionID from $TableName order by dateTimeStamp desc limit 100";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        $stmt->bind_result($UserNames, $DateTimeStamps, $Messages, $SessionID);
+
+        while($stmt->fetch())
+        {
+            if($SessionID == $sessionID)
+            {
+                echo "
+                    <p class=\"chat-usernames currentUserName\">$UserNames</p>
+                    <p class=\"currentUser\">$Messages</p>
+                    <span class=\"datetime\">$DateTimeStamps</span>";
+            }
+            else
+            {
+                echo "
+                    <p class=\"chat-usernames\">$UserNames</p>
+                    <p class=\"otherUsers\">$Messages</p>
+                    <span class=\"datetime\">$DateTimeStamps</span>";
+            }
+        }
+        $stmt->close();
+    }
+}
+
+?>
