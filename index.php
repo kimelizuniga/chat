e260b4d7a91dca00f472411888b7365f4c108338
Kim Zuniga <kim.eli.zuniga@gmail.com>
2021-11-11 22:20:26 -0500

    Add main and DisplayChat function

--- index.php
+++ index.php
@@ -10,8 +10,18 @@
 
     // MAIN
     WriteHeaders("Chat App");
-    $mysqlObj->GetData($conn);
-    echo "Hello there heroku, Kim here";
+    
+    if(isset($_POST["f_Start"]))
+        DisplayChat($mysqlObj, $sessionID);
+    else 
+        if (isset($_POST["f_Send"]))
+            DisplayChat($mysqlObj, $sessionID);
+        else
+            if(isset($_POST["f_Home"]))
+                DisplayMainPage($mysqlObj, $sessionID);
+            else
+                DisplayMainPage($mysqlObj, $sessionID);
+
     WriteFooters();
 
     // END OF MAIN
@@ -33,6 +43,42 @@
             DisplayButton("f_Start", "Start");
         echo "</div>";
         echo "</form>";
+    }
+
+    function DisplayChat(&$mysqlObj, &$sessionID)
+    {
+        $mysqlObj->CreateConnection($conn);
+        $userName = $_POST["f_Username"];
+        $sessionID = $_POST["f_SessionID"];
+        $expire = time()+60*60*24*30;          
+        setcookie("userID", $sessionID, $expire,'/');
+        $dateTimeStamp = date('Y-m-d') . " " . date('H:i:s');
+
+        if (isset($_POST["f_Send"]))
+            $mysqlObj->SendData($conn);
+
+        echo "<div id=\"container\" class=\"container\">
+            <a class=\"backBtn\" href=\"/chat/index.php\">
+                <i class=\"fas fa-chevron-circle-left\"></i>
+            </a>
+            <form action=\"index.php\" method=\"POST\">
+                <h2>Welcome <span class=\"userName\">$userName</span></h2>";
+                    DisplayInput("text", "f_Username", 15, $userName, "", "userNameText");
+                    DisplayInput("text", "f_SessionID", 15, $sessionID, "", "sessionID");
+        echo"   <div class=\"datapair\">
+                    <textarea name=\"f_Message\" id=\"message\" cols=\"50\" rows=\"4\" placeholder=\"Type here...\" autofocus></textarea>
+                    <button name=\"f_Send\" id=\"sendBtn\">Send</button>";
+        echo "  </div>";
+        echo "
+            </form>
+            <div class=\"chatContainer\"><div id=\"chat\" class=\"chat\">";
+            $mysqlObj->GetData($conn);
+        echo "</div></div>";
+        $stmt->close();
+        echo "</div>";
+        
+        echo "<script src=\"https://code.jquery.com/jquery-3.6.0.js\" integrity=\"sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=\" crossorigin=\"anonymous\"></script>";
+        echo "<script src=\"scripts/index.js\"></script>";
     }
     
 ?>
