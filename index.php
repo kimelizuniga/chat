4499ea5c7829dff95e4e9e99aa9fcaa9dd03fffa
Kim Zuniga <kim.eli.zuniga@gmail.com>
2021-11-12 16:18:48 -0500

    Add script in main file

--- index.php
+++ index.php
@@ -83,3 +83,34 @@
     }
     
 ?>
+
+<script type="text/javascript">
+    let chatDiv = document.getElementById('chat')
+    let sendBtn = document.getElementById('sendBtn')
+    let textArea = document.getElementById('message')
+
+    textArea.addEventListener("keyup", function(event){
+
+        if (event.key == 'Enter' && ! event.shiftKey) {
+            sendBtn.click()
+        }
+    })
+
+    $(document).ready(function() {
+        
+        setInterval(refreshMessage, 1000);
+        function refreshMessage(){
+            $.ajax({
+                url: 'chat.php',
+                type: 'GET',
+                dataType: 'html',
+                success: function(data) {
+                    $('#chat').html(data);
+                },
+                error: function() {
+                    $('#chat').prepend('Error retrieving new messages..');
+                }
+            });
+        }
+    });
+</script>
