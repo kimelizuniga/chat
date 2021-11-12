6f27d905299333cb744f0452fd5df32bfb62fc21
Kim Zuniga <kim.eli.zuniga@gmail.com>
2021-11-11 22:52:28 -0500

    Fix css and js

--- index.php
+++ index.php
@@ -82,7 +82,7 @@
         echo "</div>";
         
         echo "<script src=\"https://code.jquery.com/jquery-3.6.0.js\" integrity=\"sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=\" crossorigin=\"anonymous\"></script>";
-        echo "<script src=\"./scripts/index.js\"></script>";
+        echo "<script src=\"/scripts/index.js\"></script>";
     }
     
 ?>
