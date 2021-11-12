a1b32885aed9e1d95e4fee810559d2b5acb6ad67
Kim Zuniga <kim.eli.zuniga@gmail.com>
2021-11-12 08:48:38 -0500

    Add composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1f6769fc5980b1f0e3ea3bfe9e845e1fc072af69',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1f6769fc5980b1f0e3ea3bfe9e845e1fc072af69',
+            'dev_requirement' => false,
+        ),
+    ),
+);
