7288afb9dfc830d6bcc39a5e50894b882847c1e6
Kim Zuniga <kim.eli.zuniga@gmail.com>
2021-11-14 22:33:28 -0500

    Change database connection

--- clsSQLConnection.php
+++ clsSQLConnection.php
@@ -4,21 +4,21 @@ class clsSQLConnection
 {
     public function CreateConnection(&$conn)
     {
-    // //Get Heroku ClearDB connection information
-    // $cleardb_url            = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    // $cleardb_server         = $cleardb_url["host"];
-    // $cleardb_username       = $cleardb_url["user"];
-    // $cleardb_password       = $cleardb_url["pass"];
-    // $cleardb_db             = substr($cleardb_url["path"], 1);
+    // Get Heroku ClearDB connection information
+    $cleardb_url            = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server         = $cleardb_url["host"];
+    $cleardb_username       = $cleardb_url["user"];
+    $cleardb_password       = $cleardb_url["pass"];
+    $cleardb_db             = substr($cleardb_url["path"], 1);
 
-    // $active_group = 'default';
-    // $query_builder = TRUE;
+    $active_group = 'default';
+    $query_builder = TRUE;
 
-    // // Connect to DB
-    // $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    // Connect to DB
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-    // // Connect to local DB
-    $conn = new mysqli ("localhost", "root", "mysql", "chat");
+    // Connect to local DB
+    // $conn = new mysqli ("localhost", "root", "mysql", "chat");
     }
 
     public function GetData($conn)
